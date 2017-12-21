4aaf3cbd21044d4d74876efe4bd34d173655b9de
Maya Mathur <mmathur@stanford.edu>
2017-12-21 17:23:47 -0500

    Fix error sanitization

--- Shiny app/bias_factor/server.R
+++ Shiny app/bias_factor/server.R
@@ -4,9 +4,11 @@ function(input, output, session) {
   #### Compute the bias factor ####
   bias.factor <- reactive({
     # MM: reject missing or impossible inputs
-    if ( is.na( input$effect.estimate.page2 ) | is.na( input$RR_UD ) | is.na( input$RR_EU ) ) stop("Provide all three inputs.")
-    if ( any( c( input$RR_UD, input$RR_EU ) < 1 ) ) stop("RR_UD and RR_EU must be at least 1.")
-    if ( input$effect.estimate.page2 < 0 ) stop("Point estimate cannot be negative.")
+    # safeError means that these specific errors, not the generic one, will be displayed when app is published
+    #  for some reason, shiny.sanitize.errors = FALSE didn't work here
+    if ( is.na( input$effect.estimate.page2 ) | is.na( input$RR_UD ) | is.na( input$RR_EU ) ) stop( safeError("Provide all three inputs.") )
+    if ( any( c( input$RR_UD, input$RR_EU ) < 1 ) ) stop( safeError("RR_UD and RR_EU must be at least 1.") )
+    if ( input$effect.estimate.page2 < 0 ) stop( safeError("Point estimate cannot be negative.") )
   
     input$RR_UD*input$RR_EU / (input$RR_UD + input$RR_EU - 1)  
   })
@@ -20,7 +22,7 @@ function(input, output, session) {
   
   output$Bias_Factor <- renderUI({
     
-    HTML(paste0("The bias factor is ", round(bias.factor(), 2), ". At most, this bias factor could shift the risk ratio to ",
+    HTML(paste0("The bias factor is ", round(bias.factor(), 2), ". At most, this bias factor could attenuate the risk ratio to ",
                 round(adjusted.effect(), 2), "."))
   })
 }
