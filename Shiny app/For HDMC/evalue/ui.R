2741ac1a22fa22ed6d628989fd92aa65835bd0cb
Maya Mathur <mmathur@stanford.edu>
2018-09-07 08:41:27 -0700

    Update citations

--- Shiny app/For HDMC/evalue/ui.R
+++ Shiny app/For HDMC/evalue/ui.R
@@ -20,20 +20,20 @@ navbarPage( "E-value calculator", id = "navbar",
                                       on the risk ratio scale that an unmeasured confounder would need to have with both the exposure
                                       and the outcome, conditional on the measured covariates, to fully explain away a specific
                                       exposure-outcome association. Note that for outcome types other than relative risks, assumptions
-                                      are involved with the approximate conversions used. See second citation above for details.',
+                                      are involved with the approximate conversions used. See citation (2) for details.',
 
-                                      "Alternatively, you can consider the confounding strength capable of moving the observed
+                                      'Alternatively, you can consider the confounding strength capable of moving the observed
                                       association to any other value (e.g. attenuating the observed association to a true causal
                                       effect that is no longer scientifically important, or alternatively increasing a near-null
                                       observed association to a value that is of scientific importance). For this purpose, simply
-                                      type a non-null effect size into the box 'True causal effect to which to shift estimate'
-                                      when computing the E-value.",
+                                      type a non-null effect size into the box "True causal effect to which to shift estimate"
+                                      when computing the E-value.',
                                         
                                       "<b>Computing a bias factor</b>",                     
                             
                                       "Additionally, if you have substantive knowledge on the strength of the relationships
                                       between the unmeasured confounder(s) and the exposure and outcome, you can use these
-                                      numbers to <a href='https://mmathur.shinyapps.io/bias_factor/'>calculate the bias factor</a>.",
+                                      numbers to <a href='https://bias-factor.hmdc.harvard.edu/app/'>calculate the bias factor</a>.",
                                       sep="<br/><br/>")) ),
 
                           width=6
@@ -44,7 +44,7 @@ navbarPage( "E-value calculator", id = "navbar",
                         HTML(paste("<b>Please use the following citations:</b>",
                                    
                                                "<a href='https://journals.lww.com/epidem/Citation/publishahead/Website_and_R_Package_for_Computing_E_Values.98679.aspx'>(1) Mathur MB, Ding P, Riddell CA, VanderWeele TJ. (2018). Website and R package
-                                               for computing E-values. <i>Epidemiology</i>, in press.",
+                                               for computing E-values. <i>Epidemiology</i>, 29(5), e45-e47.",
                                    
                                                "<a href='http://annals.org/aim/article-abstract/2643434/sensitivity-analysis-observational-research-introducing-e-value?doi=10.7326%2fM16-2607'>(2) VanderWeele TJ,
                                                & Ding P. (2017). Sensitivity analysis in observational research: introducing the
