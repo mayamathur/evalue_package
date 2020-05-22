185a78b470f0bec6084babd69e0f202eadf5d51a
Louisa Smith <louisahsmith@gmail.com>
2020-05-22 09:51:55 -0400

    copy docs to main directory to be able to see on github pages

---
+++ docs/extra.js
@@ -0,0 +1,44 @@
+  $(".toggle").click(function() {
+    $(this).toggleClass("open");
+  });
+  
+  $(document).ready(function() {
+  
+    $chunks = $('.fold');
+  
+    $chunks.each(function () {
+  
+      // add button to source code chunks
+      if ( $(this).hasClass('s') ) {
+        $('pre.r', this).prepend("<div class=\"showopt\"><b>Show code</b></div>");
+        $('pre.r', this).children('code').attr('class', 'folded');
+      }
+  
+      // add button to output chunks
+      if ( $(this).hasClass('o') ) {
+        $('pre:not(.r)', this).has('code').prepend("<div class=\"showopt\"><b>Show table</b></div>");
+        $('pre:not(.r)', this).children('code:not(r)').addClass('folded');
+  
+        // add button to plots
+        $(this).find('img').wrap('<pre class=\"plot\"></pre>');
+        $('pre.plot', this).prepend("<div class=\"showopt\"><b>Show plot</b></div>");
+        $('pre.plot', this).children('img').addClass('folded');
+  
+      }
+    });
+  
+    // hide all chunks when document is loaded
+    $('.folded').css('display', 'none')
+  
+    // function to toggle the visibility
+    $('.showopt').click(function() {
+      var label = $(this).html();
+      if (label.indexOf("Show") >= 0) {
+        $(this).html(label.replace("Show", "Hide"));
+      } else {
+        $(this).html(label.replace("Hide", "Show"));
+      }
+      $(this).siblings('code, img').slideToggle('fast', 'swing');
+    });
+  });
+
