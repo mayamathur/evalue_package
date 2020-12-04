2d041fca667abaea4b6d000f00dcf5101d6dc67a
Maya Mathur <mmathur@stanford.edu>
2020-12-04 05:57:52 -0800

    aesthetic tweaks to docs

--- Aux code/issues.R
+++ Aux code/issues.R
@@ -5,16 +5,3 @@ detach("package:EValue")
 # also search "@@"
 
 
-setwd("~/Dropbox/Personal computer/Independent studies/Permanent websites/Metasens website/metasens_website (git)/metasens_website/Main site/tests_human_inspection")
-d = read.csv("Datasets for website test/flegal_prepped.csv")
-
-
-# moderate proportion, but no CI
-confounded_meta(method= "calibrated",
-                q = log(0.9),
-                muB = 0,
-                tail = "below",
-                yi.name = "yi",
-                vi.name = "vi",
-                dat = d,
-                R = 500)
